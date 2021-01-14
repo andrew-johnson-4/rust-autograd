63380db4a89f2adde1daab5e277e0009b15fb3ad
Ryo ASAKURA <janne.dc.ib.sa@gmail.com>
2021-01-15 01:04:13 +0900

    .

--- src/lib.rs
+++ src/lib.rs
@@ -142,7 +142,9 @@ pub extern crate ndarray;
 // extern crate intel_mkl_sys;
 
 // For other blas impl
-#[cfg(feature = "blas")]
+#[cfg(all(feature = "blas", feature = "openblas"))]
+extern crate openblas_src;
+#[cfg(all(feature = "blas", not(feature = "openblas")))]
 extern crate blas_src;
 #[cfg(feature = "blas")]
 extern crate cblas_sys;
